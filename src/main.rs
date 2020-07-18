57cfd6c0d8c5f0d5b46ac19de78ccf6c593db109
James Nguyen <jnnguyen705@gmail.com>
2020-07-17 23:57:39 -0400

    cleaned up main

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,9 @@ fn main() {
     println!("Contents:\n {}", contents);
 
     let symbol_vec:VecDeque<lexer::Token> = lexer::parse_string(&contents);
-    //println!("{:?}", &symbol_vec);
-    //lexer::print_tokens(symbol_vec);
 
     let parsed_prog:parser::prog = parser::parse_program(symbol_vec);
-
-/*     let mut test = String::from("aaaaa");
-    while(!(test.is_empty()))
-    {
-        test = test.replacen("a", "", 1);
-        println!("{}", test);
-    } */
-
+    
     codegen::generate_code(parsed_prog);
 } 
 
